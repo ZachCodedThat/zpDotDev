340cee0d4ca310e46258687004b2dce26331b152
ZachCodedThat <Zachemailsback@gmail.com>
2022-02-16 11:53:24 -0500

    Added Navbar to main splash page.
    just leaving encouraging notes for myself. remember Zach
    You can do this!!! you are great!!!

--- pages/index.js
+++ pages/index.js
@@ -22,16 +22,13 @@ import {
 } from "@styles/colorModeStyles";
 
 import { motion } from "framer-motion";
-import DarkModeSwitchNA from "@components/DarkModeSwitchNA";
+import DarkModeSwitchNA from "@components/Navbar/DarkModeSwitchNA";
+import Navbar from "@components/Navbar";
 
 const Home = () => {
-  const MotionButton = motion(Button);
-
   const { width } = useWindowDimensions();
 
   const { colorMode } = useColorMode();
-
-  console.log("rendered");
 
   return (
     <>
@@ -74,71 +71,7 @@ const Home = () => {
               Front End Web Developer
             </Heading>
           </Box>
-
-          <Flex
-            flexDirection={"row"}
-            justifyContent={"center"}
-            alignSelf={"center"}
-            marginTop="-10"
-          >
-            <Flex flexDirection={{ base: "column", md: "row" }}>
-              <Link href="./about" passHref>
-                <MotionButton
-                  color={textColor[colorMode]}
-                  variant="nav"
-                  whileTap={{ scale: 0.9 }}
-                  _hover={{
-                    bg: buttonHoverColor[colorMode],
-                    color: buttonTextHoverColor[colorMode],
-                  }}
-                >
-                  About
-                </MotionButton>
-              </Link>
-              <Link href="https://zacharyp.blog/">
-                <MotionButton
-                  color={textColor[colorMode]}
-                  variant="nav"
-                  whileTap={{ scale: 0.9 }}
-                  _hover={{
-                    bg: buttonHoverColor[colorMode],
-                    color: buttonTextHoverColor[colorMode],
-                  }}
-                >
-                  Blog
-                </MotionButton>
-              </Link>
-            </Flex>
-            <Flex flexDirection={{ base: "column", md: "row" }}>
-              <Link href="/projects" passHref>
-                <MotionButton
-                  color={textColor[colorMode]}
-                  whileTap={{ scale: 0.9 }}
-                  variant="nav"
-                  _hover={{
-                    bg: buttonHoverColor[colorMode],
-                    color: buttonTextHoverColor[colorMode],
-                  }}
-                >
-                  Projects
-                </MotionButton>
-              </Link>
-              <Link href="/contact" passHref>
-                <MotionButton
-                  color={textColor[colorMode]}
-                  variant="nav"
-                  whileTap={{ scale: 0.9 }}
-                  _hover={{
-                    bg: buttonHoverColor[colorMode],
-                    color: buttonTextHoverColor[colorMode],
-                  }}
-                >
-                  Contact
-                </MotionButton>
-              </Link>
-            </Flex>
-            {width > 600 ? <DarkModeSwitchNA /> : null}
-          </Flex>
+          <Navbar />
           {width < 600 ? <DarkModeSwitchNA /> : null}
         </Stack>
       </Container>
